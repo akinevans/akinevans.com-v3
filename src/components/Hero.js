8757c58e0057db7eed038b107c4687d4f3173c20
Akin Evans <akinevans@gmail.com>
2023-05-22 10:18:46 -0700

    Add animations to Hero component, Update Styling

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { motion } from "framer-motion";
 import akin from "../assets/images/akin-evans-vector-dark-mode.svg";
 import frontend from "../assets/images/front-end-vector-dark-mode.svg";
 import developer from "../assets/images/developer-vector-dark-mode.svg";
@@ -12,14 +13,54 @@ export default function Hero() {
     <section className='hero-outer-wrapper' id='hero-component'>
       <div className='hero-inner-wrapper'>
         <div className='hero-textSVG-wrapper'>
-          <img src={akin} alt='akin' />
-          <img src={frontend} alt='frontend' />
-          <img src={developer} alt='developer' />
+          <motion.div
+            className='akinevans'
+            initial={{ opacity: 0, y: 100 }}
+            animate={{ y: 0, x: 0, opacity: 1 }}
+            transition={{ ease: "easeOut", duration: 0.6, delay: 0 }}
+          >
+            <img src={akin} alt='akin' />
+          </motion.div>
+          <motion.div
+            className='frontend'
+            initial={{ opacity: 0, y: 100 }}
+            animate={{ y: 0, x: 0, opacity: 1 }}
+            transition={{ ease: "easeOut", duration: 0.6, delay: 0.2 }}
+          >
+            <img src={frontend} alt='frontend' />
+          </motion.div>
+          <motion.div
+            className='developer'
+            initial={{ opacity: 0, y: 100 }}
+            animate={{ y: 0, x: 0, opacity: 1 }}
+            transition={{ ease: "easeOut", duration: 0.6, delay: 0.4 }}
+          >
+            <img src={developer} alt='developer' />
+          </motion.div>
         </div>
-        <img className='profile-picture' src={profile_image} alt='Akin Evans' />
-
-        <img className='semicircle' src={semicircle} alt='Akin Evans' />
-        <img className='red-triangle' src={red_triangle} alt='red triangles' />
+        <motion.div
+          initial={{ opacity: 0 }}
+          animate={{ opacity: 1 }}
+          transition={{ ease: "easeOut", duration: 0.6, delay: 1 }}
+        >
+          <img
+            className='profile-picture'
+            src={profile_image}
+            alt='Akin Evans'
+          />
+          <img className='semicircle' src={semicircle} alt='Akin Evans' />
+        </motion.div>
+        <motion.div
+          initial={{ opacity: 0 }}
+          animate={{ opacity: 1 }}
+          transition={{ ease: "easeOut", duration: 0.6, delay: 1.4 }}
+        >
+          <img
+            className='red-triangle'
+            src={red_triangle}
+            alt='red triangles'
+          />
+        </motion.div>
       </div>
     </section>
   );
